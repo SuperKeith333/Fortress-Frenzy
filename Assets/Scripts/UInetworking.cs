c66d9b68c5d2db15947901ba237591c9de30881d
Obvious Plays <oz@abramovich.net>
2023-08-03 14:26:28 -0400

    More Not Working GUI

--- Assets/Scripts/UInetworking.cs
+++ Assets/Scripts/UInetworking.cs
@@ -1,19 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.VR;
 using UnityEngine;
 
 public class UInetworking : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int JoinGameClick = 0;
+
+    private string RoomCode;
+
+    public GameObject LoadGameBT;
+    public GameObject JoinGameTXT;
+    public GameObject LeaveGameBT;
+    public GameObject BackBT;
+
     void Start()
     {
-        
+        PhotonVRManager.Connect();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(JoinGameClick == 2)
+        {
+            JoinPublicRoom();
+        }
+    }
+
+    public void BT_JoinGame()
+    {
+        LoadGameBT.SetActive(false);
+        LeaveGameBT.SetActive(false);
+        JoinGameTXT.SetActive(true);
+        BackBT.SetActive(true);
+        JoinGameClick += 1;
+    }
+
+    public void BT_Back()
+    {
+        LoadGameBT.SetActive(true);
+        LeaveGameBT.SetActive(true);
+        JoinGameTXT.SetActive(false);
+        BackBT.SetActive(false);
+        JoinGameClick = 0;
+    }
+
+    public void GetRoomCode(string code)
+    {
+        RoomCode = code;
+        Debug.Log(code);
+    }
+
+
+    public void JoinPublicRoom()
+    {
+        //PhotonVRManager.JoinPrivateRoom(RoomCode);
+        //PhotonVRManager.SwitchScenes(2);
+        Debug.Log("worked!");
     }
 
     public void BT_LeaveGame()
